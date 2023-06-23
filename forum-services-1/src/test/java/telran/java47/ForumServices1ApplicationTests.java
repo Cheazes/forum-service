f273fd58587f130774d7677303b7e3256be85560
Petr Volozhyn <107763233+Cheazes@users.noreply.github.com>
2023-06-23 12:21:38 +0300

    Add files via upload

---
+++ forum-services-1/src/test/java/telran/java47/ForumServices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.java47;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumServices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
